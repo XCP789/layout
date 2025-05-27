c523ea1b3e490358ed0628d651ab3908f2e77878
Arturo ED <xcorpio789@gmail.com>
2025-05-27 00:28:57 -0700

    Se implemento correctamente el cambio de tema de la interfaz

--- layout-master/layout/Configuracion.xaml.cs
+++ layout-master/layout/Configuracion.xaml.cs
@@ -1,3 +1,6 @@
+using layout.Utils;
+using System.Globalization;
+
 namespace layout;
 
 public partial class Configuracion : ContentPage
@@ -5,8 +8,17 @@ public partial class Configuracion : ContentPage
     public Configuracion()
     {
         InitializeComponent();
+        // Configurar el evento de cambio de tema
+        temaSwitch.IsToggled = ConfiguracionApp.ObtenerTema();
     }
+    private void OnTemaToggled(object sender, ToggledEventArgs e)
+    {
+        ConfiguracionApp.GuardarTema(e.Value);
 
+        Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+
+        lblEstado.IsVisible = true;
+    }
     private async void OnLogoutTapped(object sender, EventArgs e)
     {
         
